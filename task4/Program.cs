153d442d5ca4d1dc003e516a84418d52135eb1b9
Andrey Vedischev <vedischev76@mail.ru>
2022-07-01 22:27:44 +0500

    add task 4

---
+++ task4/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число строк массива: ");
+int m = int.Parse(ReadLine());
+Write("Введите число столбцов массива: ");
+int n = int.Parse(ReadLine());
+int[,] arrayMain = GetRandomArray(m, n);
+PrintArray(arrayMain);
+WriteLine("----------------");
